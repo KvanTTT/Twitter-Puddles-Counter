129e919d7d6cf4902eda7c64db3e2d6cdfd00977
KvanTTT <kvanttt@gmail.com>
2013-10-31 14:54:27 +0400

    Initial commit.

---
+++ TwitterPuddlesCounter.Generator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace TwitterPuddlesCounter.Generator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 8; // Max count of walls
+			int h = 8; // Max wall height
+			byte[] answers = GenerateAnswers(n, h);
+			File.WriteAllBytes(@"..\..\..\answers.bin", Compress(answers));
+		}
+
+		static byte[] GenerateAnswers(int n, int h)
+		{
+			byte[] levels = new byte[n];
+			int hn = (int)Math.Pow(h, n);
+			byte[] result = new byte[hn];
+
+			for (int i = 0; i < hn; i++)
+			{
+				result[i] = (byte)Area(levels, n);
+				Increment(levels, n, h);
+			}
+
+			return result;
+		}
+
+		static int Area(byte[] x, int n)
+		{
+			int s = 0;
+			for (int a = 0, b = n - 1; a < b; )
+			{
+				int ha = x[a], hb = x[b];
+				if (ha < hb)
+					while (x[++a] < ha)
+						s += ha - x[a];
+				else
+					while (x[--b] < hb)
+						s += hb - x[b];
+			}
+			return s;
+		}
+
+		static int Increment(byte[] arr, int n, int h)
+		{
+			int flag = 1;
+			for (int j = 0; j < n; j++)
+			{
+				int level1 = arr[j] + flag;
+				flag = level1 / h;
+				arr[j] = (byte)(level1 % h);
+			}
+			return flag;
+		}
+
+		static byte[] Compress(byte[] raw)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress, true))
+				{
+					gzip.Write(raw, 0, raw.Length);
+				}
+				return memory.ToArray();
+			}
+		}
+	}
+}
---
+++ TwitterPuddlesCounter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace TwitterPuddlesCounter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 8; // Max count of walls
+			int h = 8; // Max wall height
+
+			byte[] ansewers = Decompress(File.ReadAllBytes(@"..\..\..\answers.bin"));
+
+			Console.WriteLine(1 == GetVolume(new byte[] { 1, 0, 1 }, ansewers, n, h));
+			Console.WriteLine(5 == GetVolume(new byte[] { 5, 0, 5 }, ansewers, n, h));
+			Console.WriteLine(1 == GetVolume(new byte[] { 0, 1, 0, 1, 0 }, ansewers, n, h));
+			Console.WriteLine(1 == GetVolume(new byte[] { 1, 0, 1, 0 }, ansewers, n, h));
+			Console.WriteLine(3 == GetVolume(new byte[] { 1, 0, 1, 2, 0, 2 }, ansewers, n, h));
+			Console.WriteLine(1 == GetVolume(new byte[] { 5, 1, 0, 1 }, ansewers, n, h));
+
+			Console.ReadLine();
+		}
+
+		static int GetVolume(byte[] levels, byte[] answers, int n, int h)
+		{
+			int ind = 0;
+			int a = 1;
+			for (int i = 0; i < Math.Min(n, levels.Length); i++)
+			{
+				ind += levels[i] * a;
+				a *= h;
+			}
+
+			return answers[ind];
+		}
+
+		static byte[] Decompress(byte[] gzip)
+		{
+			using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
+			{
+				const int size = 4096;
+				byte[] buffer = new byte[size];
+				using (MemoryStream memory = new MemoryStream())
+				{
+					int count = 0;
+					do
+					{
+						count = stream.Read(buffer, 0, size);
+						if (count > 0)
+						{
+							memory.Write(buffer, 0, count);
+						}
+					}
+					while (count > 0);
+					return memory.ToArray();
+				}
+			}
+		}
+	}
+}
